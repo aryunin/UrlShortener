6d62d001c7d8f91534f87c8cff37610a62d9db05
Artem Yunin <im.aryunin@gmail.com>
2022-09-04 13:18:30 +0300

    Application configured

--- src/main/java/ru/aryunin/UrlShortener/UrlShortenerApplication.java
+++ src/main/java/ru/aryunin/UrlShortener/UrlShortenerApplication.java
@@ -3,11 +3,16 @@ package ru.aryunin.UrlShortener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 public class UrlShortenerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UrlShortenerApplication.class, args);
+		SpringApplication springApplication = new SpringApplication(UrlShortenerApplication.class);
+		if(Arrays.asList(args).contains("-d")) springApplication.setAdditionalProfiles("inmemory");
+		else springApplication.setAdditionalProfiles("postgres");
+		springApplication.run(args);
 	}
 
 }
