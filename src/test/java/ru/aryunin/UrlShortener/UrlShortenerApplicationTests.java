b28a2908e2d5d92f1bf640478350d66cebe73b78
Artem Yunin <im.aryunin@gmail.com>
2022-09-04 12:39:56 +0300

    Initial commit

---
+++ src/test/java/ru/aryunin/UrlShortener/UrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.aryunin.UrlShortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
