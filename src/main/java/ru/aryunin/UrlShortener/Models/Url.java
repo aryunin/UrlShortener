b2639fe5ebdfd3d9b34e0fd06a04a4be574eb89c
Artem Yunin <im.aryunin@gmail.com>
2022-09-05 22:41:05 +0300

    Url modified

--- src/main/java/ru/aryunin/UrlShortener/Models/Url.java
+++ src/main/java/ru/aryunin/UrlShortener/Models/Url.java
@@ -3,11 +3,12 @@ package ru.aryunin.UrlShortener.Models;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import java.io.Serializable;
 
 @Entity
-public class ShortUrl implements Serializable {
+public class Url {
     @Id
+    @Column(name = "id")
+    private long id;
     @Column(name = "long_url")
     private String longUrl;
     @Column(name = "short_url")
@@ -27,5 +28,13 @@ public class ShortUrl implements Serializable {
 
     public void setShortUrl(String shortUrl) {
         this.shortUrl = shortUrl;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 }
--- src/main/java/ru/aryunin/UrlShortener/Repositories/ShortUrlsRepository.java
+++ src/main/java/ru/aryunin/UrlShortener/Repositories/ShortUrlsRepository.java
@@ -2,8 +2,8 @@ package ru.aryunin.UrlShortener.Repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import ru.aryunin.UrlShortener.Models.ShortUrl;
+import ru.aryunin.UrlShortener.Models.Url;
 
 @Repository
-public interface ShortUrlsRepository extends JpaRepository<ShortUrl, String> {
+public interface ShortUrlsRepository extends JpaRepository<Url, String> {
 }
