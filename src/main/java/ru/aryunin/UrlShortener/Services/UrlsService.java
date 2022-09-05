8b29f4fa5f4cdf4c806affb3c4e3de0bcda73659
Artem Yunin <im.aryunin@gmail.com>
2022-09-06 00:22:50 +0300

    ShortUrlNotFoundException throwing added

--- src/main/java/ru/aryunin/UrlShortener/Services/UrlsService.java
+++ src/main/java/ru/aryunin/UrlShortener/Services/UrlsService.java
@@ -1,13 +1,14 @@
 package ru.aryunin.UrlShortener.Services;
 
 import com.google.common.hash.Hashing;
-import org.hashids.Hashids;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.aryunin.UrlShortener.Models.Url;
 import ru.aryunin.UrlShortener.Repositories.UrlsRepository;
+import ru.aryunin.UrlShortener.Utils.ShortUrlNotFoundException;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Optional;
 
 @Service
 @Transactional(readOnly = true)
@@ -29,7 +30,7 @@ public class UrlsService {
 
     private String shortenUrl(String longUrl) {
         String fullHash = Hashing.sha256().hashString(longUrl, StandardCharsets.UTF_8).toString();
-        StringBuilder partHash = new StringBuilder("");
+        StringBuilder partHash = new StringBuilder();
         partHash.append(fullHash.charAt(0));
         for(int i = 1; i < fullHash.length() && urlsRepository.findByShortUrl(partHash.toString()).isPresent(); i++)
             partHash.append(fullHash.charAt(i));
@@ -37,6 +38,8 @@ public class UrlsService {
     }
 
     public String getLongUrl(String shortUrl) {
-        return urlsRepository.findByShortUrl(shortUrl).get().getLongUrl(); // TODO
+        Optional<Url> url = urlsRepository.findByShortUrl(shortUrl);
+        if(url.isPresent()) return url.get().getLongUrl();
+        else throw new ShortUrlNotFoundException("That short URL does not exist!");
     }
 }
